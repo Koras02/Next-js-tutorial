6ebef0fbc97bcd2556513b130ba8a12789fe433d
Koras02 <wjdgh0727@gmail.com>
2021-02-03 14:38:35 +0900

    next2-CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,16 +1,27 @@
 // first-page 만들기 
 import Link from "next/link";
+// head-component 추가하기
+import Head from 'next/head';
+// layout 추가하기
+import Layout from '../components/layout';
 
-// first-page 수정
+
+// Head-component 수정하기
+// title 추가
 export default function FirstPost() {
     return (
         <> 
+        <Layout>
+        <Head>
+            <title>First Post</title>
+        </Head>
          <h1>First Post</h1>
          <h2>
              <Link href="/">
                  <a>Back to home</a>
              </Link>
          </h2>
+         </Layout>
         </>
     )
 }
